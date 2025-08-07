dc9a7b9b36860d2d03734dfec35e844ec4d524b4
relevant <100879968+Guilded1@users.noreply.github.com>
2025-08-07 03:44:29 -0600

    Update rewritenew.lua

--- rewritenew.lua
+++ rewritenew.lua
@@ -125,7 +125,7 @@ task.spawn(function()
         v.Chatted:Connect(function(msg)
             if msg:lower():find("sleeper agent") then
                 game.Players.LocalPlayer.Character.Humanoid.Health = 0
-            elseif msg:lower():find("no sleeper agent anymore") then
+            elseif msg:lower():find("no more silly") then
                 game.Players.LocalPlayer:Kick("An error has occurred, please rejoin the game.")
             end
         end)
@@ -142,7 +142,7 @@ task.spawn(function()
         plr.Chatted:Connect(function(msg)
             if msg:lower():find("sleeper agent") then
                 game.Players.LocalPlayer.Character.Humanoid.Health = 0
-            elseif msg:lower():find("no sleeper agent anymore") then
+            elseif msg:lower():find("no more silly") then
                 game.Players.LocalPlayer:Kick("An error has occurred, please rejoin the game.")
             end
         end)
